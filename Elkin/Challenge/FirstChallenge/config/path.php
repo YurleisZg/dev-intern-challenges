5569898e67dac02ee19f37a05f2221004ca3e4bd
Elkin Vasquez <elkindavidvasquezrivera801@gmail.com>
2025-12-13 12:07:43 -0500

    add: first challenge database config

---
+++ Elkin/Challenge/FirstChallenge/config/path.php
@@ -0,0 +1,8 @@
+<?php
+
+const BASE_URL = '/dev-intern-challenges/Elkin/Challenge/FirstChallenge/';
+const PUBLIC_CSS = 'public/css/style.css';
+const AUTH_CSS = 'view/login/auth.css';
+const CHALLENGE_URL = '/dev-intern-challenges/Elkin/Challenge/index.php';
+
+
