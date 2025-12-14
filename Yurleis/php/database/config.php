0ea1359b1a3f14758c3516cc0480131cc9bba57d
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-12-14 18:45:57 -0500

    feat: remove deprecated user registration and To Do list files

--- Yurleis/php/database/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = "localhost";
-$databaseUsername = 'your_username';
-$databasePassword = 'your_password';
-$databaseName = 'name_of_database';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
