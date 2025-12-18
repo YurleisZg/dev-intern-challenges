c4233747905150de95b86ed5e940b42607208510
Elkin Vasquez <elkindavidvasquezrivera801@gmail.com>
2025-12-18 17:25:52 -0500

    Fix: problem docker src

--- Elkin/Challenge/FirstChallenge/config/path.php
+++ Elkin/Challenge/FirstChallenge/config/path.php
@@ -1,8 +1,6 @@
 <?php
 
-const BASE_URL = '/dev-intern-challenges/Elkin/Challenge/FirstChallenge/';
+const BASE_URL = '/Elkin/Challenge/FirstChallenge';
 const PUBLIC_CSS = 'public/css/style.css';
 const AUTH_CSS = 'view/login/auth.css';
-const CHALLENGE_URL = '/dev-intern-challenges/Elkin/Challenge/index.php';
-
-
+const CHALLENGE_URL = '/Elkin/Challenge/index.php';
